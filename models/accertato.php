eccfe94e3732e9cd1221deaf2d041ca58eea65d3
Nico Didonna <101733253+nicodidonna@users.noreply.github.com>
2022-12-01 11:11:15 +0100

    update Istat

--- models/accertato.php
+++ models/accertato.php
@@ -22,22 +22,24 @@ class Accertato
             if($tipoRead == 'verbali'){
 
                 //query
-                $query = "SELECT a.Imp_Ridotto_60gg_euro, b.spese_notifica_verbale_bollettario, b.spese_stampa_verbale_bollettario , b.tipo_bollettario
-                FROM $this->table_name_1 as b
+                $query = "SELECT istat.Imp_Ridotto_60gg_euro, b.spese_notifica_verbale_bollettario, b.spese_stampa_verbale_bollettario , b.tipo_bollettario
+                FROM db2_bollettario as b
                 INNER JOIN db2_infrazione as i on i.id_bollettario_infrazione = b.id_bollettario
                 INNER JOIN articoli_new as a on a.id_articolo = i.Cod_Articolo_infrazione
-                WHERE b.stato_archivio_verbale_bollettario = 0 AND a.Imp_Ridotto_60gg IS NOT NULL AND a.Imp_Ridotto_60gg_euro IS NOT NULL";
+                INNER JOIN istat2021 as istat on istat.id_articolo = a.id_articolo
+                WHERE b.stato_archivio_verbale_bollettario = 0 AND istat.Imp_Ridotto_60gg_euro IS NOT NULL";
 
             }
 
             if($tipoRead == 'preavvisi'){
 
                 //query
-                $query = "SELECT a.Imp_Ridotto_60gg_euro
-                FROM $this->table_name_2 as b
+                $query = "SELECT istat.Imp_Ridotto_60gg_euro
+                FROM db6_bollettario_pr as b
                 INNER JOIN db2_infrazione as i on i.id_bollettario_infrazione = b.id_bollettario_pr
                 INNER JOIN articoli_new as a on a.id_articolo = i.Cod_Articolo_infrazione
-                WHERE b.stato_archivio_verbale_bollettario_pr = 0 AND a.Imp_Ridotto_60gg IS NOT NULL AND a.Imp_Ridotto_60gg_euro IS NOT NULL; ";
+                INNER JOIN istat2021 as istat on istat.id_articolo = a.id_articolo
+                WHERE b.stato_archivio_verbale_bollettario_pr = 0 AND istat.Imp_Ridotto_60gg_euro IS NOT NULL";
 
             }
             
