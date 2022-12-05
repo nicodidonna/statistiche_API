7699065c9f942a4cc4fde091d3624453b4b713c2
Nico Didonna <101733253+nicodidonna@users.noreply.github.com>
2022-12-05 15:03:05 +0100

    verbaliByAgente preavvisi

--- models/verbali_agente.php
+++ models/verbali_agente.php
@@ -22,29 +22,65 @@ class VerbaliAgente
     }
 
     // READ verbali_by_articolo
-    function read($dataInizio = null, $dataFine = null)
+    function read($tipoRead, $dataInizio = null, $dataFine = null)
     {
 
-        // select all
-        if ($dataInizio != null and $dataFine != null) {
-            $query = "SELECT a.nome_agente, a.cognome_agente, a.grado_agente, a.matricola_agente, count(b.id_bollettario) as num_verbali
-                      FROM $this->table_name_1 as b
-                      INNER JOIN $this->table_name_2 as a on b.id_agente_assegn_bollettario = a.id_agente
-                      WHERE CAST(b.data_verbale_bollettario AS DATE) BETWEEN '$dataInizio' AND '$dataFine' AND b.stato_archivio_verbale_bollettario = 0
-                      GROUP BY a.matricola_agente
-                      ORDER BY num_verbali DESC";
-        } else {
-            $query = "SELECT a.nome_agente, a.cognome_agente, a.grado_agente, a.matricola_agente, count(b.id_bollettario) as num_verbali
-                      FROM $this->table_name_1 as b
-                      INNER JOIN $this->table_name_2 as a on b.id_agente_assegn_bollettario = a.id_agente
-                      WHERE CAST(b.data_verbale_bollettario AS DATE) <= CURDATE() AND b.stato_archivio_verbale_bollettario = 0
-                      GROUP BY a.matricola_agente
-                      ORDER BY num_verbali DESC";
+        if($tipoRead == 'verbali'){
+            
+            // select all
+            if ($dataInizio != null and $dataFine != null) {
+                
+                $query = "SELECT a.nome_agente, a.cognome_agente, a.grado_agente, a.matricola_agente, count(b.id_bollettario) as num_verbali
+                FROM $this->table_name_1 as b
+                INNER JOIN $this->table_name_2 as a on b.id_agente_assegn_bollettario = a.id_agente
+                WHERE CAST(b.data_verbale_bollettario AS DATE) BETWEEN '$dataInizio' AND '$dataFine' AND b.stato_archivio_verbale_bollettario = 0
+                GROUP BY a.matricola_agente
+                ORDER BY num_verbali DESC";
+                
+            } else {
+                
+                $query = "SELECT a.nome_agente, a.cognome_agente, a.grado_agente, a.matricola_agente, count(b.id_bollettario) as num_verbali
+                FROM $this->table_name_1 as b
+                INNER JOIN $this->table_name_2 as a on b.id_agente_assegn_bollettario = a.id_agente
+                WHERE CAST(b.data_verbale_bollettario AS DATE) <= CURDATE() AND b.stato_archivio_verbale_bollettario = 0
+                GROUP BY a.matricola_agente
+                ORDER BY num_verbali DESC";
+                
+            }
+        
         }
+
+        if($tipoRead == 'preavvisi'){
+
+            // select all
+            if ($dataInizio != null and $dataFine != null) {
+                
+                $query = "SELECT a.nome_agente, a.cognome_agente, a.grado_agente, a.matricola_agente, count(b.id_bollettario_pr) as num_verbali
+                FROM db6_bollettario_pr as b
+                INNER JOIN db1_agente as a on b.id_agente_assegn_bollettario_pr = a.id_agente
+                WHERE CAST(b.data_verbale_bollettario_pr AS DATE) BETWEEN '$dataInizio' AND '$dataFine' AND b.stato_archivio_verbale_bollettario_pr = 0
+                GROUP BY a.matricola_agente
+                ORDER BY num_verbali DESC";
+                
+            } else {
+                
+                $query = "SELECT a.nome_agente, a.cognome_agente, a.grado_agente, a.matricola_agente, count(b.id_bollettario_pr) as num_verbali
+                FROM db6_bollettario_pr as b
+                INNER JOIN db1_agente as a on b.id_agente_assegn_bollettario_pr = a.id_agente
+                WHERE CAST(b.data_verbale_bollettario_pr AS DATE) <= CURDATE() AND b.stato_archivio_verbale_bollettario_pr = 0
+                GROUP BY a.matricola_agente
+                ORDER BY num_verbali DESC";
+                
+            }
+
+        }
+        
+        
         $stmt = $this->conn->prepare($query);
         // execute query
         $stmt->execute();
         return $stmt;
+    
     }
 
 }
--- verbali_agente/read.php
+++ verbali_agente/read.php
@@ -11,36 +11,67 @@ include_once '../models/verbali_agente.php';
 // Creiamo un nuovo oggetto VerbaliAgente e passiamoli la connessione
 $verbali_agente = new VerbaliAgente();
 
-if (isset($_GET['data_inizio']) and isset($_GET['data_fine']) ) {
-    //prendo i parametri dall'url
-    $param = $_GET['data_inizio'];
-    $param2 = $_GET['data_fine'];
-    $stmt = $verbali_agente->read($param, $param2);
-
-} else {
-
-    $stmt = $verbali_agente->read();
+//prendo dai parametri GET la richiesta di verbali o preavvisi e faccio la read in base a quello
+if( isset($_GET['tipoRead']) ){
+    
+    $GLOBALS['tipoRead'] = $_GET['tipoRead'];
+    
+    if ( isset($_GET['data_inizio']) and isset($_GET['data_fine']) ) {
+        
+        //prendo i parametri dall'url
+        $param = $_GET['data_inizio'];
+        $param2 = $_GET['data_fine'];
+        
+        $stmt = $verbali_agente->read($GLOBALS['tipoRead'],$param,$param2);
+    
+    } else {
+        
+        $stmt = $verbali_agente->read($GLOBALS['tipoRead']);
+    
+    }
 
 }
 
 // query products
 $num = $stmt->rowCount();
 
-// se vengono trovati verbali nel database
+// se ci sono righe di risultato nel database
 if ($num > 0) {
 
-    // array di verbali
+    // array di risultati
     $verbali_arr = array();
 
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $table_item = array(
-            "nome_agente" => $row['nome_agente'],
-            "cognome_agente" => $row['cognome_agente'],
-            "grado_agente" => $row['grado_agente'],
-            "matricola_agente" => $row['matricola_agente'],
-            "num_verbali" => $row['num_verbali']
-        );
+
+        //se chiedo verbali riempio un l'array temporaneo $table_item con i valori del risultato della query
+        if($GLOBALS['tipoRead'] == 'verbali'){
+            
+            $table_item = array(
+                "nome_agente" => $row['nome_agente'],
+                "cognome_agente" => $row['cognome_agente'],
+                "grado_agente" => $row['grado_agente'],
+                "matricola_agente" => $row['matricola_agente'],
+                "num_verbali" => $row['num_verbali']
+            );
+
+        }
+
+
+        //se chiedo verbali riempio un l'array temporaneo $table_item con i valori del risultato della query
+        if($GLOBALS['tipoRead'] == 'preavvisi'){
+            
+            $table_item = array(
+                "nome_agente" => $row['nome_agente'],
+                "cognome_agente" => $row['cognome_agente'],
+                "grado_agente" => $row['grado_agente'],
+                "matricola_agente" => $row['matricola_agente'],
+                "num_verbali" => $row['num_verbali']
+            );
+
+        }
+        
         array_push($verbali_arr, $table_item);
+
     }
 
     http_response_code(200);
