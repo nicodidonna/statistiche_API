b7633c90c171c75c73db2d575921610d41ba519d
Nico Didonna <101733253+nicodidonna@users.noreply.github.com>
2022-12-29 11:34:54 +0100

    cast date verbali pag/parz.pag/non pag.

--- models/verbali_non_pagati.php
+++ models/verbali_non_pagati.php
@@ -21,13 +21,13 @@ class VerbaliNonPagati
             // select all
             if ($dataInizio != null and $dataFine != null) {
                 
-                $query = "SELECT b.num_ordine_bollettario AS cronologico, b.numero_bollettario AS numero_verbale, b.anno_bollettario AS anno_verbale, b.stato_bollettario AS stato_verbale, b.data_verbale_bollettario AS data_verbale
+                $query = "SELECT b.num_ordine_bollettario AS cronologico, b.numero_bollettario AS numero_verbale, b.anno_bollettario AS anno_verbale, b.stato_bollettario AS stato_verbale, CAST(b.data_verbale_bollettario AS DATE) AS data_verbale
                 FROM db2_bollettario AS b
                 WHERE b.stato_pagamento_verbale_bollettario = 1 AND b.data_pagamento_verbale_bollettario IS NULL AND b.stato_archivio_verbale_bollettario = 0 AND CAST(b.data_verbale_bollettario AS DATE) BETWEEN '$dataInizio' AND '$dataFine' ";
                 
             } else {
                 
-                $query = "SELECT b.num_ordine_bollettario AS cronologico, b.numero_bollettario AS numero_verbale, b.anno_bollettario AS anno_verbale, b.stato_bollettario AS stato_verbale, b.data_verbale_bollettario AS data_verbale
+                $query = "SELECT b.num_ordine_bollettario AS cronologico, b.numero_bollettario AS numero_verbale, b.anno_bollettario AS anno_verbale, b.stato_bollettario AS stato_verbale, CAST(b.data_verbale_bollettario AS DATE) AS data_verbale
                 FROM db2_bollettario AS b
                 WHERE b.stato_pagamento_verbale_bollettario = 1 AND b.data_pagamento_verbale_bollettario IS NULL AND b.stato_archivio_verbale_bollettario = 0 AND CAST(b.data_verbale_bollettario AS DATE) <=CURDATE()";
                 
@@ -40,13 +40,13 @@ class VerbaliNonPagati
             // select all
             if ($dataInizio != null and $dataFine != null) {
                 
-                $query = "SELECT b.num_ordine_bollettario_pr AS cronologico, b.numero_bollettario_pr AS numero_verbale, b.anno_bollettario_pr AS anno_verbale, b.stato_bollettario_pr AS stato_verbale, b.data_verbale_bollettario_pr AS data_verbale
+                $query = "SELECT b.num_ordine_bollettario_pr AS cronologico, b.numero_bollettario_pr AS numero_verbale, b.anno_bollettario_pr AS anno_verbale, b.stato_bollettario_pr AS stato_verbale, CAST(b.data_verbale_bollettario_pr AS DATE) AS data_verbale
                 FROM db6_bollettario_pr AS b
                 WHERE b.stato_pagamento_verbale_bollettario_pr = 1 AND b.data_pagamento_verbale_bollettario_pr IS NULL AND b.stato_archivio_verbale_bollettario_pr = 0 AND CAST(b.data_verbale_bollettario_pr AS DATE) BETWEEN '$dataInizio' AND '$dataFine' ";
                 
             } else {
                 
-                $query = "SELECT b.num_ordine_bollettario_pr AS cronologico, b.numero_bollettario_pr AS numero_verbale, b.anno_bollettario_pr AS anno_verbale, b.stato_bollettario_pr AS stato_verbale, b.data_verbale_bollettario_pr AS data_verbale
+                $query = "SELECT b.num_ordine_bollettario_pr AS cronologico, b.numero_bollettario_pr AS numero_verbale, b.anno_bollettario_pr AS anno_verbale, b.stato_bollettario_pr AS stato_verbale, CAST(b.data_verbale_bollettario_pr AS DATE) AS data_verbale
                 FROM db6_bollettario_pr AS b
                 WHERE b.stato_pagamento_verbale_bollettario_pr = 1 AND b.data_pagamento_verbale_bollettario_pr IS NULL AND b.stato_archivio_verbale_bollettario_pr = 0 AND CAST(b.data_verbale_bollettario_pr AS DATE) <=CURDATE()";
                 
