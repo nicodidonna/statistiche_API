441a1d2a200c63687752c6464a3fbada1dfb5d87
Nico Didonna <101733253+nicodidonna@users.noreply.github.com>
2022-12-30 10:00:42 +0100

    tipo_sanzione_accessoria descrizione

--- verbali_sanzioneacc/read.php
+++ verbali_sanzioneacc/read.php
@@ -56,6 +56,101 @@ if($num>0){
         
         //se chiedo verbali riempio un l'array temporaneo $table_item con i valori del risultato della query
         if($GLOBALS['tipoRead'] == 'verbali'){
+
+            switch ($row['tipo_sanzione_accessoria']) {
+
+                case '100':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione carta di circolazione con il ritiro del documento";
+                    break;
+
+                case '101':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della licenza";
+                    break;
+                    
+                case '102':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione della patente di guida senza il ritiro del documento";
+                    break;
+                    
+                case '103':
+                    $row['tipo_sanzione_accessoria'] = "Confisca amministrativa del veicolo";
+                    break;
+                    
+                case '104':
+                    $row['tipo_sanzione_accessoria'] = "Obbligo delle rimozioni delle opere abusive";
+                    break;
+                    
+                case '105':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione dell'autorizzazione";
+                    break;
+                    
+                case '106':
+                    $row['tipo_sanzione_accessoria'] = "Revoca della patente di guida";
+                    break;
+                    
+                case '107':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della patente di guida";
+                    break;
+                    
+                case '110':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione della patente di guida con ritiro del documento";
+                    break;
+                    
+                case '111':
+                    $row['tipo_sanzione_accessoria'] = "Obbligo di ripristino dei luoghi";
+                    break;
+                    
+                case '112':
+                    $row['tipo_sanzione_accessoria'] = "Confisca amministrativa di oggetti effetto della violazione";
+                    break;
+                    
+                case '113':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della targa";
+                    break;
+                    
+                case '114':
+                    $row['tipo_sanzione_accessoria'] = "Sequestro amministrativo del veicolo";
+                    break;
+                    
+                case '116':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione carta di circolazione senza il ritiro del documento";
+                    break;
+                    
+                case '117':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della carta di circolazione";
+                    break;
+                    
+                case '118':
+                    $row['tipo_sanzione_accessoria'] = "Segnalazione patente";
+                    break;
+                    
+                case '119':
+                    $row['tipo_sanzione_accessoria'] = "Fermo amministrativo";
+                    break;
+                    
+                case '120':
+                    $row['tipo_sanzione_accessoria'] = "Alienazione veicolo";
+                    break;
+                    
+                case '121':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per rottamazione";
+                    break;
+                    
+                case '122':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per riattivazione assicurazione entro 30 giorni";
+                    break;
+                    
+                case '123':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per pagamento assicurazione";
+                    break;
+                    
+                case '124':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro con targa prova";
+                    break;    
+                
+                default:
+                    $row['tipo_sanzione_accessoria'] = "Tipo di sanzione accessoria non riconosciuto";
+                    break;
+            }
             
             $table_item = array(
                 "tipo_sanzione_accessoria" => $row['tipo_sanzione_accessoria'],
@@ -66,6 +161,101 @@ if($num>0){
         
         //se chiedo preavvisi riempio un l'array temporaneo $table_item con i valori del risultato della query
         if($GLOBALS['tipoRead'] == 'preavvisi'){
+
+            switch ($row['tipo_sanzione_accessoria']) {
+
+                case '100':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione carta di circolazione con il ritiro del documento";
+                    break;
+
+                case '101':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della licenza";
+                    break;
+                    
+                case '102':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione della patente di guida senza il ritiro del documento";
+                    break;
+                    
+                case '103':
+                    $row['tipo_sanzione_accessoria'] = "Confisca amministrativa del veicolo";
+                    break;
+                    
+                case '104':
+                    $row['tipo_sanzione_accessoria'] = "Obbligo delle rimozioni delle opere abusive";
+                    break;
+                    
+                case '105':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione dell'autorizzazione";
+                    break;
+                    
+                case '106':
+                    $row['tipo_sanzione_accessoria'] = "Revoca della patente di guida";
+                    break;
+                    
+                case '107':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della patente di guida";
+                    break;
+                    
+                case '110':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione della patente di guida con ritiro del documento";
+                    break;
+                    
+                case '111':
+                    $row['tipo_sanzione_accessoria'] = "Obbligo di ripristino dei luoghi";
+                    break;
+                    
+                case '112':
+                    $row['tipo_sanzione_accessoria'] = "Confisca amministrativa di oggetti effetto della violazione";
+                    break;
+                    
+                case '113':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della targa";
+                    break;
+                    
+                case '114':
+                    $row['tipo_sanzione_accessoria'] = "Sequestro amministrativo del veicolo";
+                    break;
+                    
+                case '116':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione carta di circolazione senza il ritiro del documento";
+                    break;
+                    
+                case '117':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della carta di circolazione";
+                    break;
+                    
+                case '118':
+                    $row['tipo_sanzione_accessoria'] = "Segnalazione patente";
+                    break;
+                    
+                case '119':
+                    $row['tipo_sanzione_accessoria'] = "Fermo amministrativo";
+                    break;
+                    
+                case '120':
+                    $row['tipo_sanzione_accessoria'] = "Alienazione veicolo";
+                    break;
+                    
+                case '121':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per rottamazione";
+                    break;
+                    
+                case '122':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per riattivazione assicurazione entro 30 giorni";
+                    break;
+                    
+                case '123':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per pagamento assicurazione";
+                    break;
+                    
+                case '124':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro con targa prova";
+                    break;    
+                
+                default:
+                    $row['tipo_sanzione_accessoria'] = "Tipo di sanzione accessoria non riconosciuto";
+                    break;
+            }
             
             $table_item = array(
                 "tipo_sanzione_accessoria" => $row['tipo_sanzione_accessoria'],
@@ -76,6 +266,101 @@ if($num>0){
 
         //se chiedo verbali e preavvisi riempio un l'array temporaneo $table_item con i valori del risultato della query
         if($GLOBALS['tipoRead'] == 'verbali_preavvisi'){
+
+            switch ($row['tipo_sanzione_accessoria']) {
+
+                case '100':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione carta di circolazione con il ritiro del documento";
+                    break;
+
+                case '101':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della licenza";
+                    break;
+                    
+                case '102':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione della patente di guida senza il ritiro del documento";
+                    break;
+                    
+                case '103':
+                    $row['tipo_sanzione_accessoria'] = "Confisca amministrativa del veicolo";
+                    break;
+                    
+                case '104':
+                    $row['tipo_sanzione_accessoria'] = "Obbligo delle rimozioni delle opere abusive";
+                    break;
+                    
+                case '105':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione dell'autorizzazione";
+                    break;
+                    
+                case '106':
+                    $row['tipo_sanzione_accessoria'] = "Revoca della patente di guida";
+                    break;
+                    
+                case '107':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della patente di guida";
+                    break;
+                    
+                case '110':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione della patente di guida con ritiro del documento";
+                    break;
+                    
+                case '111':
+                    $row['tipo_sanzione_accessoria'] = "Obbligo di ripristino dei luoghi";
+                    break;
+                    
+                case '112':
+                    $row['tipo_sanzione_accessoria'] = "Confisca amministrativa di oggetti effetto della violazione";
+                    break;
+                    
+                case '113':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della targa";
+                    break;
+                    
+                case '114':
+                    $row['tipo_sanzione_accessoria'] = "Sequestro amministrativo del veicolo";
+                    break;
+                    
+                case '116':
+                    $row['tipo_sanzione_accessoria'] = "Sospensione carta di circolazione senza il ritiro del documento";
+                    break;
+                    
+                case '117':
+                    $row['tipo_sanzione_accessoria'] = "Ritiro della carta di circolazione";
+                    break;
+                    
+                case '118':
+                    $row['tipo_sanzione_accessoria'] = "Segnalazione patente";
+                    break;
+                    
+                case '119':
+                    $row['tipo_sanzione_accessoria'] = "Fermo amministrativo";
+                    break;
+                    
+                case '120':
+                    $row['tipo_sanzione_accessoria'] = "Alienazione veicolo";
+                    break;
+                    
+                case '121':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per rottamazione";
+                    break;
+                    
+                case '122':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per riattivazione assicurazione entro 30 giorni";
+                    break;
+                    
+                case '123':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro per pagamento assicurazione";
+                    break;
+                    
+                case '124':
+                    $row['tipo_sanzione_accessoria'] = "Dissequestro con targa prova";
+                    break;    
+                
+                default:
+                    $row['tipo_sanzione_accessoria'] = "Tipo di sanzione accessoria non riconosciuto";
+                    break;
+            }
             
             $table_item = array(
                 "tipo_sanzione_accessoria" => $row['tipo_sanzione_accessoria'],
