45d57fc2ab148ee14f6a87800f5e3282a2c6e6b7
Nico Didonna <101733253+nicodidonna@users.noreply.github.com>
2023-01-10 09:16:13 +0100

    fix query verbali by preavviso

--- models/verbali_by_preavviso.php
+++ models/verbali_by_preavviso.php
@@ -28,16 +28,18 @@ class VerbaliPreavviso
 			if ($dataInizio != null and $dataFine != null) {
 				
 				$query = "SELECT b.num_ordine_bollettario AS cronologico, b.numero_bollettario AS numero_verbale, b.anno_bollettario AS anno_verbale, DATE_FORMAT(b.data_verbale_bollettario,'%d/%m/%Y') AS data_verbale, a.Descrizione AS articolo
-				FROM db2_bollettario AS b
+				FROM db6_asspr AS ass
+				INNER JOIN db2_bollettario AS b ON b.id_bollettario = ass.id_bollettario_asspr 
 				INNER JOIN db2_infrazione AS i ON b.id_bollettario = i.id_bollettario_infrazione
 				INNER JOIN articoli_new AS a ON i.Cod_Articolo_infrazione = a.id_articolo
-				WHERE b.stato_archivio_verbale_bollettario = 0 AND CAST(b.data_verbale_bollettario AS DATE) between '$dataInizio' and '$dataFine' 
+				WHERE b.stato_archivio_verbale_bollettario = 0 AND CAST(b.data_verbale_bollettario AS DATE) BETWEEN '$dataInizio' AND '$dataFine'
 				ORDER BY b.data_verbale_bollettario DESC";
 				
 			} else {
 				
 				$query = "SELECT b.num_ordine_bollettario AS cronologico, b.numero_bollettario AS numero_verbale, b.anno_bollettario AS anno_verbale, DATE_FORMAT(b.data_verbale_bollettario,'%d/%m/%Y') AS data_verbale, a.Descrizione AS articolo
-				FROM db2_bollettario AS b
+				FROM db6_asspr AS ass
+				INNER JOIN db2_bollettario AS b ON b.id_bollettario = ass.id_bollettario_asspr 
 				INNER JOIN db2_infrazione AS i ON b.id_bollettario = i.id_bollettario_infrazione
 				INNER JOIN articoli_new AS a ON i.Cod_Articolo_infrazione = a.id_articolo
 				WHERE b.stato_archivio_verbale_bollettario = 0 AND CAST(b.data_verbale_bollettario AS DATE) <= CURDATE() 
